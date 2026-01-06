17a9e86e6ff724f773ee908a67b7f3fcea1cdfda
Jesus <jesusperez_14@hotmail.com>
2026-01-06 12:33:51 +0100

    favicon apple

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",        
     shortcut: "/favicon.ico",
-    apple: "/favicon.png", 
+    apple: "/apple-touch-icon.png", 
   },
   openGraph: {
     title: "Cerebrando - Transforma tu mente con neurociencia",
